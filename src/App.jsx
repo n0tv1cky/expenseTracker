f5267c59e5d724dbea218bf332d9633c3eaa6e75
Vikhyathraj Shetty K <itsvikhyathraj@gmail.com>
2023-08-16 22:24:45 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Stack } from "react-bootstrap"
+import Container from "react-bootstrap/Container"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <Container className='my-4'>
+    <Stack direction='horizontal' gap={2} className='mb-4'>
+      <h1 className='me-auto'>Budgets</h1>
+      <Button variant='primary'>Add Budget</Button>
+      <Button variant='outline-primary'>Add Expense</Button>
+    </Stack>
+    <div style={{display:"grid", 
+                  gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", 
+                  gap: "1rem", 
+                  alignItems: "flex-start"}}>
+    </div>
+    <BudgetCard></BudgetCard>
+  </Container>
 }
 
 export default App
